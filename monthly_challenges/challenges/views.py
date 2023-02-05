cce9a3298a3c0bef8e1141dfc3a2fe15dea8e036
Coder Partha <80641510+parthadeori@users.noreply.github.com>
2023-02-05 10:46:37 +0530

    Adding More Dynamic View Logic

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,20 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
+
+monthly_challenges = {
+    "january": "Eat no meat for the entire month.",
+    "february": "Walk for atleast 20 minutes every day.",
+    "march": "Learn Django for atleast 20 minutes every day.",
+    "april": "Eat 7-9 cups of veggies every day.",
+    "may": "Do brain training exercises.",
+    "june": "Cook a new recipe every day.",
+    "july": "Sleep for 8-9 hours every night.",
+    "august": "Walk 10,000 steps every day.",
+    "september": "Follow a productivity system.",
+    "october": "Follow a morning routine.",
+    "november": "Follow a bedtime routine.",
+    "december": "Check email once or twice a day."
+}
+
 # Create your views here.
 
 def monthly_challenges_by_number(request, month):
     return HttpResponse(month)
 
 
-def monthly_challenges(request, month):
-    challenge_text = None,
-    if month == "january":
-        challenge_text = "Eat no meat for the entire month."
-    elif month == "february":
-        challenge_text = "Walk for atleast 20 minutes everyday."
-    elif month == "march":
-        challenge_text = "Learn Django for atleast 20 minutes everyday."
-    else:
+
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
         return HttpResponseNotFound("This month is not supported.")
-    return HttpResponse(challenge_text)
